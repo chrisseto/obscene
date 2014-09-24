12540000be20c36bfcbfb73302a42564a0b4decc
Chris Seto <chriskseto@gmail.com>
2014-09-23 23:38:37 -0400

    Allow GesturePoint to export a Point

--- core/src/main/scala/GesturePoint.scala
+++ core/src/main/scala/GesturePoint.scala
@@ -1,5 +1,10 @@
 package xyz.seto.obscene
 
+import xyz.seto.obscene.utils.Point
+
+
 class GesturePoint(val x: Float, val y: Float, val timeStamp: Long) {
+  def point = new Point(x, y)
+
   override def toString: String = s"<GesturePoint ($x, $y) $timeStamp>"
 }
