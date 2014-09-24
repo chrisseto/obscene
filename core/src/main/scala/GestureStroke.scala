c4bd78ff26dc7a30ba979ec7f549ecd8e69153fe
Chris Seto <chriskseto@gmail.com>
2014-09-23 23:39:10 -0400

    Adds the boundingBox variable for gestureStroke

--- core/src/main/scala/GestureStroke.scala
+++ core/src/main/scala/GestureStroke.scala
@@ -1,7 +1,15 @@
 package xyz.seto.obscene
 
-class GestureStroke(val points: List[GesturePoint]){
+import xyz.seto.obscene.utils.Rectangle
 
+
+class GestureStroke(val points: List[GesturePoint]){
+  private def createBoundingBox(cBox: Rectangle, cPoints: List[GesturePoint]): Rectangle = cPoints match {
+      case cP :: cPoints => createBoundingBox(cBox.union(cP.point), cPoints)
+      case Nil => cBox
+  }
+
+  val boundingBox: Rectangle = createBoundingBox(new Rectangle(0, 0), points)
 
 }
 
