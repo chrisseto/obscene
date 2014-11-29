c3be4b59c5baf7ab46d11eff02c6c5ae6834397b
Chris Seto <chriskseto@gmail.com>
2014-11-29 00:03:38 -0500

    Add utility method

--- core/src/main/scala/utils/Rectangle.scala
+++ core/src/main/scala/utils/Rectangle.scala
@@ -1,11 +1,15 @@
 package xyz.seto.obscene.utils
 
+import scala.math.{pow, sqrt}
+
 
 class Rectangle(val cords: Point, val dims: Point) {
   def this(x: Float, y: Float, width: Float, height: Float) = this(new Point(x, y), new Point(width, height))
   def this(width: Float, height: Float) = this(0, 0, width, height)
 
   val topCorner: Point = cords + dims
+
+  val diagonal: Float = sqrt(pow(dims.x, 2) + pow(dims.y, 2)).toFloat
 
   private def scaleDims(p: Point): Point = topCorner.scaleUp(p) - cords
 
@@ -18,5 +22,13 @@ class Rectangle(val cords: Point, val dims: Point) {
   def translate(x: Float, y: Float): Rectangle = translate(new Point(x, y))
 
   def translate(p: Point): Rectangle = new Rectangle(cords + p, dims)
+
+  def width(): Float = dims.x
+
+  def height(): Float = dims.y
+
+  def centerX(): Float = dims.x / 2
+
+  def centerY(): Float = dims.y / 2
 
 }
