9f7e63e4da3fbe504fb3d898563d1e8070162eca
Chris Seto <chriskseto@gmail.com>
2014-11-28 23:42:34 -0500

    Add a prediction class

---
+++ core/src/main/scala/Prediction.scala
@@ -0,0 +1,10 @@
+package xyz.seto.obscene;
+
+class Prediction(val name: String, val score: Double) {
+  
+  def >(that: Prediction): Boolean = that.score > this.score
+
+  def <(that: Prediction): Boolean = !(that > this)
+
+  override def toString(): String = s"<Prediction $name ($score)>"
+}
