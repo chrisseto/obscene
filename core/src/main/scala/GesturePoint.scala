e1ae1791ada7462bfa95e165859b3b3d225051ad
Chris Seto <chriskseto@gmail.com>
2014-11-29 00:02:06 -0500

    Stub method for completeness

--- core/src/main/scala/GesturePoint.scala
+++ core/src/main/scala/GesturePoint.scala
@@ -6,5 +6,7 @@ import xyz.seto.obscene.utils.Point
 class GesturePoint(val x: Float, val y: Float, val timeStamp: Long) {
   def point = new Point(x, y)
 
+  def serialize():(Float, Float, Long) = (x, y, timeStamp)
+
   override def toString: String = s"<GesturePoint ($x, $y) $timeStamp>"
 }
