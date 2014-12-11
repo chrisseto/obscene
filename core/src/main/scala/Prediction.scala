cccf9bd731301ac347255fe7eceba4aed965f2fe
Chris Seto <chriskseto@gmail.com>
2014-12-11 00:04:10 -0500

    Make Prediction sortable

--- core/src/main/scala/Prediction.scala
+++ core/src/main/scala/Prediction.scala
@@ -1,10 +1,16 @@
 package xyz.seto.obscene;
 
-class Prediction(val name: String, val score: Double) {
+class Prediction(val name: String, val score: Double)  extends Ordered[Prediction]{
 
-  def >(that: Prediction): Boolean = that.score > this.score
+  override def >(that: Prediction): Boolean = that.score > this.score
 
-  def <(that: Prediction): Boolean = !(that > this)
+  override def <(that: Prediction): Boolean = !(that > this)
+
+  override def compare(that: Prediction): Int = {
+    if(this < that) -1
+    else if(this > that) 1
+    else 0
+  }
 
   override def toString(): String = s"<Prediction $name ($score)>"
 }
