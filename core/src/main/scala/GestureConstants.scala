36f00d9e47432cad7dfce89501eafa8f7eee4896
Chris Seto <chriskseto@gmail.com>
2014-12-10 23:59:03 -0500

    Port GestureConstants

---
+++ core/src/main/scala/GestureConstants.scala
@@ -0,0 +1,10 @@
+package xyz.seto.obscene;
+
+object GestureConstants {
+    val LOG_TAG = "Obscene";
+
+    val IO_BUFFER_SIZE = 32 * 1024; // 32K
+
+    val STROKE_POINT_BUFFER_SIZE = 100; // number of points
+    val STROKE_STRING_BUFFER_SIZE = 1024;
+}
