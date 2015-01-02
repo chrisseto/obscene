86b8c98d9e487696f37cfd86a020185a9420d2f2
Chris Seto <chriskseto@gmail.com>
2015-01-01 20:39:10 -0500

    Logic clean up

--- core/src/main/scala/Prediction.scala
+++ core/src/main/scala/Prediction.scala
@@ -4,12 +4,14 @@ class Prediction(val name: String, val score: Double)  extends Ordered[Predictio
 
   override def >(that: Prediction): Boolean = that.score > this.score
 
-  override def <(that: Prediction): Boolean = !(that > this)
+  override def <(that: Prediction): Boolean = that.score < this.score
+
+  def ~=(that: Prediction): Boolean = that.score == this.score
 
   override def compare(that: Prediction): Int = {
-    if(this < that) -1
-    else if(this > that) 1
-    else 0
+    if(this > that) 1
+    else if(this ~= that) 0
+    else -1
   }
 
   override def toString(): String = s"<Prediction $name ($score)>"
