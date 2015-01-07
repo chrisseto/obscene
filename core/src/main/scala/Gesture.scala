61a4aed6ef3d7cef581eae13ead7d81fd7d634a2
Chris Seto <chriskseto@gmail.com>
2015-01-07 00:03:44 -0500

    Some docstrings and cleanup

--- core/src/main/scala/Gesture.scala
+++ core/src/main/scala/Gesture.scala
@@ -7,32 +7,49 @@ import xyz.seto.obscene.utils.Rectangle
 
 
 class Gesture(val id: Long, val strokes: List[GestureStroke], val boundingBox: Rectangle){
-  def this() = this(0, List())
   def this(id: Long, strokes: List[GestureStroke]) =
     this(id, strokes, Gesture.computeBoundingBox(strokes))
+
+  def this() = this(0, List())
 
   def addStroke(stroke: GestureStroke): Gesture =
     new Gesture(id, strokes :+ stroke, boundingBox.union(stroke.boundingBox))
 
   def serialize(stream: DataOutput): Unit = {
-    stream.writeLong(this.id)
-    stream.writeInt(this.strokes.length)
-    this.strokes.map(x => x.serialize(stream))
+    stream writeLong id
+    stream writeInt strokes.length
+    this.strokes.map(stroke => stroke serialize stream)
   }
 
 }
 
 object Gesture {
+  /** Generate the bounding box of the entire gesture
+   * computed by unioning the boundingboxes of all the contained strokes
+   * @param strokes The list of GestureStrokes to aggregate
+   * @return The gesture's bounding box
+   */
   def computeBoundingBox(strokes: List[GestureStroke]): Rectangle = {
     def loop(strokes: List[GestureStroke], box: Rectangle): Rectangle =
       strokes match {
-        case s1 :: ss => loop(ss, box.union(s1.boundingBox))
+        case s1 :: ss => loop(ss, box union s1.boundingBox)
         case _ => box
       }
     loop(strokes, new Rectangle(0,0))
   }
 
+  /** Deserialize a gesture from the given data input
+   * A gesture is defined
+   * as an id (Long)
+   * followed by the number of strokes contained (Integer)
+   * finally followed by x strokes
+   * @return The deserialized gesture
+   */
   def deserialize(stream: DataInput): Gesture =
-    new Gesture(stream.readLong(), (for(_ <- 1 to stream.readInt())
-      yield GestureStroke.deserialize(stream)).to[List])
+    new Gesture(
+      stream.readLong(),
+      (for(_ <- 1 to stream.readInt())
+        yield GestureStroke deserialize stream
+      ).toList
+    )
 }
